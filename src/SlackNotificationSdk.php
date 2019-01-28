1a77539ce89447673deae78071899b15b545808b
felipemoura <felipemouracv@gmail.com>
2019-01-28 12:28:52 -0200

    Update SlackNotificationSdk

--- src/SlackNotificationSdk.php
+++ src/SlackNotificationSdk.php
@@ -7,21 +7,20 @@ use GuzzleHttp\Client;
 class SlackNotificationSdk
 {
     private $client = null;
+
     function __construct()
     {
-        $this->client = new Client(['base_uri'=> getenv('ENDPOINT_SLACK_NOTIFICATION')]);
-    }
-
-    public function pack()
-    {
-        return "Hello Packagist! I am coming" . getenv('ENDPOINT_SLACK_NOTIFICATION');
+        $this->client = new Client(['base_uri' => getenv('ENDPOINT_SLACK_NOTIFICATION')]);
     }
 
     public function notify($notificationIdOrSlug, $variables)
     {
-        $this->client->requestAsync('POST', "notification/".$notificationIdOrSlug."/send", [
-            'json' => $variables
-        ]);
+        try {
+            $this->client->request('POST', "notification/" . $notificationIdOrSlug . "/send", [
+                'json' => $variables
+            ]);
+        } catch (\Exception $exception) {
+        }
     }
 }
 
